75e2478c836bdf4af51568d254c0dc4c252fdbcd
Adrien MARTINEAU <contact@adrien-martineau.fr>
2025-02-24 00:57:43 +0100

    feat(guard): add redirectRoute param

--- projects/ngx-http-helper/src/lib/auth/guard/auth.guard.ts
+++ projects/ngx-http-helper/src/lib/auth/guard/auth.guard.ts
@@ -3,6 +3,8 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AUTH_FEATURE_CONFIG_TOKEN } from '../../http-helper.tokens';
 
+const unslash = (path: string) => path.replace(/^\/|\/$/g, '');
+
 export const authGuard = (redirectRoute?: string, selector: string = 'default'): CanActivateFn => () => {
   const router = inject(Router);
   const config = inject(AUTH_FEATURE_CONFIG_TOKEN);
@@ -17,11 +19,12 @@ export const authGuard = (redirectRoute?: string, selector: string = 'default'):
     throw Error(`No tokenSelector for key '${selector}'. Please declare it from the withAuth()`);
   }
 
-  const redirectUrl = router.createUrlTree([redirectRoute || config.guard?.redirectRoute || '']);
+  const redirectPath = redirectRoute || config.guard?.redirectRoute || '/';
+  const redirectUrl = router.createUrlTree([redirectPath]);
 
   return tokenSelector().pipe(
     first(),
-    map(token => !!token || redirectUrl),
+    map(token => !!token || (unslash(redirectPath) !== unslash(location.pathname) && redirectUrl)),
     catchError(() => of(redirectUrl)),
   );
 };
--- projects/testing/src/app/app.routes.ts
+++ projects/testing/src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from '../../../ngx-http-helper/src/lib/auth/guard/auth.guard';
+import { OtherComponent } from './components/other/other.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        canActivate: [authGuard('/login')]
+        canActivate: [authGuard()]
+    },
+    {
+        path: 'other',
+        component: OtherComponent,
+        canActivate: [authGuard('/other', 'other')]
     },
     {
         path: 'login',
